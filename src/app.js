e7abce0b4c4b33b9b0c8a80e3cf6a5d13ac55789
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-02-17 19:21:45 -0600

    file handling and routing

--- src/app.js
+++ src/app.js
@@ -11,10 +11,51 @@ app.use(express.static(path.join(__dirname, './public'))) //use these static fil
 app.set('views', path.join(__dirname, './views')); //set views equal to this path
 app.set('view engine', 'ejs'); //set view engine equal to ejs
 
-app.get('/', (request, response) => {
-  response.render('index', {title:'Index'});
-}); //build the root, i.e. index, route
+//read account data
+const accountData = fs.readFileSync('src/json/accounts.json','utf8');
+const accounts = JSON.parse(accountData);
+//read user data
+const userData = fs.readFileSync('src/json/users.json','utf8');
+const users = JSON.parse(userData);
 
+//index route
+app.get('/', (request, response) => {
+  response.render('index', {
+    title:'Account Summary',
+    accounts,
+  });
+}); 
+
+//account routes:
+//savings route
+app.get('/savings', (request, response) => {
+  response.render('account', {
+    account: accounts.savings
+  });
+});
+
+//credit route
+app.get('/credit', (request, response) => {
+  response.render('account', {
+    account: accounts.credit
+  });
+});
+
+//checking route
+app.get('/checking', (request, response) => {
+  response.render('account', {
+    account: accounts.checking
+  });
+});
+
+//profile route
+app.get('/profile', (request, response) => {
+  response.render('profile', {
+    user: users[0]
+  });
+});
+
+//server
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
 });
